3fcec865066bbbb9ff52cf44247c64d6a55a497d
hovse <melkon@helix.am>
2022-10-02 00:33:53 +0400

    I added log functionality in App.class

--- app/src/main/java/com/core/baseaplication/App.kt
+++ app/src/main/java/com/core/baseaplication/App.kt
@@ -2,11 +2,13 @@ package com.core.baseaplication
 
 import com.core.HelixApp
 import com.core.baseaplication.utils.initCoreConfig
+import com.core.logger.log
 
 class App : HelixApp() {
 
     override fun onCreate() {
         super.onCreate()
         initCoreConfig()
+        log { "Print Hello Gevorg" }
     }
 }
